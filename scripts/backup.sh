97690876584c3be81114568269039a28cdf5c1db
weibiao.tu <weibiao.tu@shopee.com>
2021-08-20 10:12:24 +0800

    backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 branch=hexo
-github=git@gitee.com:newtorn/newtorn.github.io.git
+github=git@github.com:newtorn/newtorn.github.io.git
 gitee=git@gitee.com:newtorn/newtorn.git
 
 set -ue
@@ -10,5 +10,5 @@ git checkout $branch
 git add .
 git commit -m "backup"
 
-echo "git push $github --force"
+git push $github --force
 git push $gitee --force
